b11467825d37db138ca5c685a42177f4b54dcecc
Sim <simshard@gmail.com>
2025-02-17 17:03:08 +0000

    architecture

---
+++ app/Models/Employer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employer extends Model
+{
+    /** @use HasFactory<\Database\Factories\EmployerFactory> */
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/Job.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    /** @use HasFactory<\Database\Factories\JobFactory> */
+    use HasFactory;
+    protected $table = 'jobs';
+
+    protected $fillable = ['title', 'salary','employer_id'];
+    //protected $guarded = [];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_id');
+    }
+}
